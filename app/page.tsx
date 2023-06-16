b441de179d583af83a6c02ba369bba6372ac6b9b
gayashan4lk <gayashan4lk@gmail.com>
2023-06-17 01:27:19 +0530

    update mex file

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import Content from './message.mdx';
 
@@ -7,26 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
 	return (
 		<main className={`${inter.className}`}>
-			<div>
-				<a
-					href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					By{' '}
-					<Image
-						src='/vercel.svg'
-						alt='Vercel Logo'
-						width={100}
-						height={24}
-						priority
-					/>
-				</a>
-			</div>
-
-			<div>
-				<Content />
-			</div>
+			<Content />
 		</main>
 	);
 }
